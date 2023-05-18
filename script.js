75dc3695ac5357943727a7b03b775c0d319a6ee3
Moidhas <moidk3026@gmail.com>
2023-05-18 17:28:47 -0400

    made .dispaly responsive

--- script.js
+++ script.js
@@ -26,23 +26,48 @@ function operate(a, operator, b) {
     }
 }
 
-console.log(operate("1", "/", "2"));
+function display(btn, zeroValue) {
+    if (zeroValue) {
+        screen.textContent = btn.textContent;
+    } else {
+        screen.textContent += btn.textContent;
+    }
+}
+
+
+
+// might not need id's
 const ops = [ {op: "+", id: "add"}, {op: "-", id: "sub"}, {op: "*", id: "prod"}, 
                 {op: "/", id: "divide"}, {op: "=", id: "equal"}];
+
+const screen = document.querySelector('.display');
 const interface = document.querySelector('.interface');
+
 const clear = document.createElement('button');
 clear.textContent = "CLEAR";
+clear.addEventListener("click", () => console.log("yo"));
 interface.appendChild(clear);
+
 for (let i = 0; i < 15; i++) {
     const btn = document.createElement('button');
     if (i < 10) {
         btn.textContent = i;
         btn.setAttribute("id", `n${i}`);
+        btn.addEventListener("click", () => {
+            if (screen.textContent === "0") {
+                display(btn, true);
+            } else {
+                display(btn, false);
+            }
+        });
+        
     } else {
         btn.textContent = ops[i - 10].op;
         btn.setAttribute("id", `${ops[i - 10].id}`);
+        btn.addEventListener("click", () => console.log("yo"));
     }
+    
     interface.appendChild(btn);
 }
 
-const buttons = document.querySelectorAll('button');
+
